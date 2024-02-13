6425072ed11e92f676d0ac3fca8fac6769e333bb
ajaydusa1 <ajay.dusa@yahoo.in>
2024-02-13 20:59:31 +0530

    VL53L1x sensor working

---
+++ Core/Src/main.c
@@ -0,0 +1,397 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "vl53l1_api.h"
+#include "vl53l1_platform.h"
+#include "vl53l1_api_calibration.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+char buff[40];
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c2;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+/* USER CODE BEGIN PV */
+VL53L1_Dev_t                   dev;
+VL53L1_DEV                     Dev = &dev;
+int status;
+volatile int IntCount;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void AutonomousLowPowerRangingTest(void); /* see Autonomous ranging example implementation in USER CODE BEGIN 4 section */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	  uint8_t byteData;
+	  uint16_t wordData;
+	  uint8_t ToFSensor = 1; // 0=Left, 1=Center(default), 2=Right
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C2_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  Dev->I2cHandle = &hi2c2;
+  Dev->I2cDevAddr = 0x52;
+
+  VL53L1_RdByte(Dev, 0x010F, &byteData);
+  printf("VL53L1X Model_ID: %02X\n\r", byteData);
+  VL53L1_RdByte(Dev, 0x0110, &byteData);
+  printf("VL53L1X Module_Type: %02X\n\r", byteData);
+  VL53L1_RdWord(Dev, 0x010F, &wordData);
+  printf("VL53L1X: %02X\n\r", wordData);
+
+  AutonomousLowPowerRangingTest();
+
+
+	/*********************************/
+/*     Sensor configuration      */
+/*********************************/
+
+/* (Optional) Program sensor to raise an interrupt ONLY below 300mm */
+//status = VL53L1X_SetDistanceMode(dev, 2);
+//	status = VL53L1X_SetInterruptPolarity(dev, 0);
+//status = VL53L1X_SetTimingBudgetInMs(dev, 100); /* in ms possible values [20, 50, 100, 200, 500] */
+//status = VL53L1X_SetInterMeasurementInMs(dev, 100); /* in ms, IM must be > = TB */
+//status = VL53L1X_SetDistanceThreshold(dev,100,300,0,1);
+
+//	status = VL53L1X_StartRanging(dev);   /* This function has to be called to enable the ranging */
+
+//printf("Ranging started. Put your hand close to the sensor to generate an interrupt...\n");
+
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  AutonomousLowPowerRangingTest();
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x00303D5B;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, RLY_Pin|BUZZ_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : VL53L1X_INT_Pin */
+  GPIO_InitStruct.Pin = VL53L1X_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(VL53L1X_INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RLY_Pin BUZZ_Pin */
+  GPIO_InitStruct.Pin = RLY_Pin|BUZZ_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI4_15_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+void AutonomousLowPowerRangingTest(void)
+	{
+		static VL53L1_RangingMeasurementData_t RangingData;
+		    status = VL53L1_WaitDeviceBooted(Dev);
+			status = VL53L1_DataInit(Dev);
+			status = VL53L1_StaticInit(Dev);
+			status = VL53L1_SetDistanceMode(Dev, VL53L1_DISTANCEMODE_LONG);
+			status = VL53L1_SetMeasurementTimingBudgetMicroSeconds(Dev, 50000);
+			status = VL53L1_SetInterMeasurementPeriodMilliSeconds(Dev, 500);
+			status = VL53L1_StartMeasurement(Dev);
+
+	  do // polling mode
+		{
+		  status = VL53L1_WaitMeasurementDataReady(Dev);
+			if(!status)
+			{
+				status = VL53L1_GetRangingMeasurementData(Dev, &RangingData);
+				if(status==0){
+
+						if(status==0){
+
+							sprintf(buff, "%d, %d  \n\r", RangingData.RangeStatus, RangingData.RangeMilliMeter);
+	            HAL_UART_Transmit(&huart2, (uint8_t*)buff, strlen(buff), 0xFFFF);
+
+							/*sprintf(buff,"VL53L1X n address is %d	\r\n",RangingData);
+						HAL_UART_Transmit(&huart2,(uint8_t*)buff,sizeof(buff),200);
+						HAL_Delay(100);*/
+
+				}
+
+
+
+				}
+				status = VL53L1_ClearInterruptAndStartMeasurement(Dev);
+			}
+		}
+		while (1);
+
+
+
+	}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Debug/Drivers/VL53L1X Drivers/API/platform/src/vl53l1_platform_log.d
@@ -0,0 +1,6 @@
+Drivers/VL53L1X Drivers/API/platform/src/vl53l1_platform_log.o: \
+ ../Drivers/VL53L1X\ Drivers/API/platform/src/vl53l1_platform_log.c \
+ E:/Projects/Electronics/Personal\ Projects/Overhead\ Tank/STM32\ Practise/Sensor\ Reading/Drivers/VL53L1X\ Drivers/API/platform/inc/vl53l1_platform_log.h \
+ E:/Projects/Electronics/Personal\ Projects/Overhead\ Tank/STM32\ Practise/Sensor\ Reading/Drivers/VL53L1X\ Drivers/API/platform/inc/vl53l1_platform_user_config.h
+E:/Projects/Electronics/Personal\ Projects/Overhead\ Tank/STM32\ Practise/Sensor\ Reading/Drivers/VL53L1X\ Drivers/API/platform/inc/vl53l1_platform_log.h:
+E:/Projects/Electronics/Personal\ Projects/Overhead\ Tank/STM32\ Practise/Sensor\ Reading/Drivers/VL53L1X\ Drivers/API/platform/inc/vl53l1_platform_user_config.h:
